c13469b0759acd3039c679467f6ca51d188a3186
torresurban@gmail.com <torresurban@gmail.com>
2021-10-02 13:23:18 -0500

    agregando el ultimo ejercicio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -234,14 +234,12 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-    if(letra = 'a' ='e'='i'='o'='u'){
-        return 'Es vocal';
-    }
-    else if (letra.length > 1){
-        return 'Dato incorrecto';
-    }
-    else{
-      return 'Dato incorrecto';
+    if(letra === 'a'  ||letra === 'e'  ||letra === 'i'  ||letra === 'o'  ||letra === ''){
+          return 'Es vocal';
+    } else if (letra.length > 1){
+          return 'Dato incorrecto';
+    } else{
+          return 'Dato incorrecto';
     }
 }
 
